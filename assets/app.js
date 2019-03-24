42596694929c8468f7f8b6ff0ebc7c3ee6a229ff
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-03-24 19:27:26 -0400

    This push is updating everything we have so far with what we've found thus far

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,18 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAJQVW0Y5UfYDyiF4udSmi8OlWvb_x2tas",
+    authDomain: "farmer-s-market-dd1bd.firebaseapp.com",
+    databaseURL: "https://farmer-s-market-dd1bd.firebaseio.com",
+    projectId: "farmer-s-market-dd1bd",
+    storageBucket: "farmer-s-market-dd1bd.appspot.com",
+    messagingSenderId: "1044283229575"
+  };
+  firebase.initializeApp(config);
+
+//---------------------------------------------------------------------------------
 //SEARCH FARMERS MARKETS BY ZIPCODE 
 //API
-
 function getResults(zip) {
     // or
     // function getResults(lat, lng) {
@@ -15,6 +27,7 @@ function getResults(zip) {
         jsonpCallback: 'searchResultsHandler'
     });
 }
+
 //iterate through the JSON result object.
 function searchResultsHandler(searchResults) {
     for (var key in searchresults) {
@@ -31,3 +44,30 @@ function searchResultsHandler(searchResults) {
         }
     }
 }
+//-------------------------------------------------------------------------
+// This makes our map work
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 35.7796, lng: -78.6382},
+          zoom: 9
+        });
+      };
+
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+      });
+    
+      // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+      // var collapsibleElem = document.querySelector('.collapsible');
+      // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+    
+      // Or with jQuery
+    
+      $(document).ready(function(){
+        $('.sidenav').sidenav();
+      });
+
+      //---------------------------------------------------------------------------
